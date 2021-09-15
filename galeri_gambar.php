e831225f527848f734c669f4cf1dcb0fbfb36bfc
Sofiandri Ahmad Yurkhaidi <87758681+SofiandriAhmadYurkhaidi@users.noreply.github.com>
2021-09-15 15:02:10 +0700

    menambahkan tugas 2
    Tugas WEB

---
+++ galeri_gambar.php
@@ -0,0 +1,65 @@
+<html>
+    <head>
+        <!-- title berfungsi untuk pemberian nama tab browser website anda-->
+        <<title> sofianrpl2 </title>
+        <!-- meta berfungsi untuk search engine (SEO) membaca konten website anda-->
+        <meta charset="UTF-8"></meta>
+        <meta name="description" content="INDEX"></meta>
+        <meta name="keywords" content="HTML,CSS,Website"></meta>
+        <meta name="author" content="SOFIANDRI AHMAD YURKHAIDI"></meta>
+    </head>
+    <body>
+        <!-- ukuran dasar / template / canvas desain website anda-->
+        <table border="1" width="1200px" height="700px" align="center">
+            <!-- baris ini untuk memasukan konten banner dengan file png/jpg-->
+            <tr height="150px">
+                <td>
+                    <img src="img/sekolah.png" width="1200px" height="300px">
+                </td>    
+            </tr>
+            <!-- baris ini untuk membuat menu-->
+            <tr height="50px" style="background-color:green;">
+                <td align="center" style="word-spacing: 4em;">
+                    <!-- dibawah ini adalah termasuk dalam jenis internal link-->
+                    <a href="index.php" style="text-decoration: none; color: white;"><strong>Beranda</strong></a>
+                    <a href="profile.php" style="text-decoration: none; color: white;">Profile</a>
+                    <a href="galeri_gambar.php" style="text-decoration: none; color: white;">Gambar</a>
+                    <a href="galeri_vidio.php" style="text-decoration: none; color: white;">Video</a>
+                    <a href="kontak.php" style="text-decoration: none; color: white;"><b>Kontak</b></a>
+                    <!-- dibawah ini adalah termasuk dalam jenis external link-->
+                    <a href="https://www.instagram.com/sofiyandri05/" style="text-decoration: none; color: white;"><em>Instagram</em></a>
+                    <a href="https://github.com/SofiandriAhmadYurkhaidi" style="text-decoration: none; color: white;"><i>GITHUB</i></a>
+                </td>    
+            </tr>
+            <!-- baris ini adalah konten dari isi website-->
+            <tr height="450px" style="background-color: green;;">
+                <td valign="top">
+                    <!-- Source code untuk memasukan konten di galeri gambar-->
+                    <table align="center" style="margin-top: 50px;">
+                        <tr>
+                            <td><img src="img/logo smkn4.png" alt="logo smkn4.png" width="200px"></td>
+                            <td><img src="img/logo smkn4.png" alt="logo smkn4.png" width="200px"></td>
+                            <td><img src="img/logo smkn4.png" alt="logo smkn4.png" width="200px"></td>
+                            <td><img src="img/logo smkn4.png" alt="logo smkn4.png" width="200px"></td>
+                            <td><img src="img/logo smkn4.png" alt="logo smkn4.png" width="200px"></td>
+                        </tr>   
+                        <tr>
+                            <td><img src="img/logo smkn4.png" alt="logo smkn4.png" width="200px"></td>
+                            <td><img src="img/logo smkn4.png" alt="logo smkn4.png" width="200px"></td>
+                            <td><img src="img/logo smkn4.png" alt="logo smkn4.png" width="200px"></td>
+                            <td><img src="img/logo smkn4.png" alt="logo smkn4.png" width="200px"></td>
+                            <td><img src="img/logo smkn4.png" alt="logo smkn4.png" width="200px"></td>
+                        </tr>   
+
+                    </table>
+                </td>    
+            </tr>
+            <!-- baris ini untuk footer-->
+            <tr height="50px" style="background-color: green;">
+                <td>
+                    <p align="center" style="color: white;">SOFIANDRI AHMAD YURKHAIDI XI-RPL2</p>
+                </td>    
+            </tr>
+        </table>
+    </body>
+</html>
---
+++ kontak.php
@@ -0,0 +1,62 @@
+<html>
+    <head>
+        <!-- title berfungsi untuk pemberian nama tab browser website anda-->
+        <title> sofianrpl2 </title>
+        <!-- meta berfungsi untuk search engine (SEO) membaca konten website anda-->
+        <meta charset="UTF-8"></meta>
+        <meta name="description" content="GALERI GAMBAR"></meta>
+        <meta name="keywords" content="HTML,CSS,Website"></meta>
+        <meta name="author" content="SOFIANDRI AHMAD YURKHAIDI"></meta>
+    </head>
+    <body>
+        <!-- ukuran dasar / template / canvas desain website anda-->
+        <table border="1" width="1200px" height="700px" align="center">
+            <!-- baris ini untuk memasukan konten banner dengan file png/jpg-->
+            <tr height="150px">
+                <td>
+                    <img src="img/sekolah.png" width="1200px" height="300px">
+                </td>    
+            </tr>
+            <!-- baris ini untuk membuat menu-->
+            <tr height="50px" style="background-color:green;">
+                <td align="center" style="word-spacing: 4em;">
+                    <!-- dibawah ini adalah termasuk dalam jenis internal link-->
+                    <a href="index.php" style="text-decoration: none; color: white;"><strong>Beranda</strong></a>
+                    <a href="profile.php" style="text-decoration: none; color: white;">Profile</a>
+                    <a href="galeri_gambar.php" style="text-decoration: none; color: white;">Gambar</a>
+                    <a href="galeri_vidio.php" style="text-decoration: none; color: white;">Vidio</a>
+                    <a href="kontak.php" style="text-decoration: none; color: white;"><b>Kontak</b></a>
+                    <!-- dibawah ini adalah termasuk dalam jenis external link-->
+                    <a href="https://www.instagram.com/sofiyandri05/" style="text-decoration: none; color: white;"><i>Instagram</i></a>
+                    <a href="https://github.com/SofiandriAhmadYurkhaidi" style="text-decoration: none; color: white;"><i>GITHUB</i></a>
+                </td>    
+            </tr>
+            <!-- baris ini adalah konten dari isi website-->
+            <tr height="450px" style="background-color: green;">
+                <td valign="top">
+                    <!-- Source code untuk memasukan konten di galeri gambar-->
+                    <table border="1" width="450px" height="300px">
+                    <tr>
+                        <th height="50px">KONTAK</th>
+                    </tr>
+                    
+                    <tr>
+                        <td>
+                            <h3>NO. HP: 083809115345</h3>
+                            <h3>EMAIL:  sofyandriyurka01@gmail.com</h3>
+                            <h3>FACEBOOK: Sofiandri</h3>
+                            <h3>IG: @sofiyandri05</h3>
+                        </td>
+                    </tr>
+                    </table>
+                </td>    
+            </tr>
+            <!-- baris ini untuk footer-->
+            <tr height="50px" style="background-color: green;">
+                <td>
+                    <p align="center" style="color: white;">SOFIANDRI AHMAD YURKHAIDI XI-RPL2</p>
+                </td>    
+            </tr>
+        </table>
+    </body>
+</html>
